4f2c67e768d608e5a1f5ad587f6de39af2517be1
ZigEliaZag <eliakaliza50@icloud.com>
2025-12-23 21:05:34 +0100

    added stylesheet to toggle menu list. added code to active and open menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("ready link")
+
+// navigation bar triger 
+const toggleMenuBtn = document.querySelector("header .btn-toggle");
+const menuList = document.querySelector("header .navigation-menu");
+
+function showMenu() {
+    /*
+     targets menu button to dipslay menu list
+    */
+    toggleMenuBtn.classList.toggle("active") // activets close menu
+    menuList.classList.toggle("open") // activets open menu
+}
+
+// display menu list
+toggleMenuBtn.addEventListener("click", showMenu)
