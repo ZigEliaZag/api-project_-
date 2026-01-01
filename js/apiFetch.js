6ebc9fa44d763cfe37c229b3a49ad79a37e31c82
ZigEliaZag <eliakaliza50@icloud.com>
2026-01-01 23:38:41 +0100

    added journey section, api configuration file

--- js/apiFetch.js
+++ js/apiFetch.js
@@ -2,14 +2,24 @@
 const londonUrL = 'https://en.wikipedia.org/api/rest_v1/page/summary/London';
 const manchesterUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/Manchester';
 
+
+
+// export function
 export async function londonSummary() {
+    /*
+    fetch summary london from wikipedia api
+    */
     const response = await fetch(londonUrL)
     const respond = await response.json()
 
     return respond.extract
 }
 
+// export function
 export async function manchesterSummary() {
+    /*
+    fetch summary manchester from wikipedia api 
+     */
     const response = await fetch(manchesterUrl)
     const respond = await response.json()
 
