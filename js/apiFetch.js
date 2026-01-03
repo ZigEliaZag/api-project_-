6205ffa0079af8ef5cc84c36804375107e7a5538
ZigEliaZag <eliakaliza50@icloud.com>
2026-01-03 01:22:58 +0100

    function to handle pexels api fetching and retrun the results

--- js/apiFetch.js
+++ js/apiFetch.js
@@ -1,3 +1,8 @@
+// import config
+import { CONFIG } from "../config/tokens.js";
+// pexels cleant
+
+
 // api summary url
 const londonUrL = 'https://en.wikipedia.org/api/rest_v1/page/summary/London';
 const manchesterUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/Manchester';
@@ -25,5 +30,38 @@ export async function manchesterSummary() {
 
     return respond.extract
 }
+
+
+// fetch pexels image
+let pexelsApiUrl = CONFIG.PEXELS_URL  // pexels photo url 
+const pexelsApiKey = CONFIG.PEXELS_KEY  // pexels personal key
+const imageid = CONFIG.IMAGE_ID // id of the image/photo
+
+export async function handlePexelsApi() {
+    /**
+     fetch the pexels api photo and return the result
+     */
+    const getData = await fetch(`${pexelsApiUrl}/${imageid}`, {
+        method: 'GET',
+        id: imageid,
+        headers: {
+            Authorization: pexelsApiKey // personal key to access the api
+        }
+    })
+    const displayData = await getData.json()
+
+    // dipslay error message if image failed to load
+    if (displayData !== displayData) {
+        console.error('Could not load', displayData)
+    } else {
+
+        //return displayData.src.original
+    }
+
+    console.log(displayData.src.original)
+}
+
+handlePexelsApi();
+
 
 
