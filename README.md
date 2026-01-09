23fd04f50280323d7c7343cced0d1da18f63a6db
ZigEliaZag <eliakaliza50@icloud.com>
2026-01-09 13:23:53 +0100

    Revert "edited files"
    This reverts commit 4c9324db65ba49cee8daad74560fec17ae1380d4.

--- README.md
+++
@@ -1,2 +0,0 @@
-### Secret key 
-To ensure that my api key does not get expose, I use CONFIG then export the key to api file 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// import api function
-import { londonSummary, manchesterSummary } from "./apiFetch.js";
-
 
 // navigation bar triger 
 const toggleMenuBtn = document.querySelector("header .btn-toggle");
@@ -16,21 +13,3 @@ function showMenu() {
 
 // display menu list
 toggleMenuBtn.addEventListener("click", showMenu)
-
-/* attuch api to new function
-async function importLondonData() {
-    const londonData = await londonSummary();
-    const manchesterData = await manchesterSummary()
-
-} abandoned function
- */
-
-// attuch api response to html body
-const londonApiSummery = await londonSummary()
-const manchesterApiSummary = await manchesterSummary()
-
-document.getElementById("londonSummary").innerHTML = londonApiSummery;
-document.getElementById("manchesterSummary").innerHTML = manchesterApiSummary
-
-
-
