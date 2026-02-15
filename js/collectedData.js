5c4a856f14b5ba398993055a43168b8c30dc9bad
ZigEliaZag <eliakaliza50@icloud.com>
2026-02-15 12:05:42 +0100

    added new js file to display api to other html files than index.html

---
+++ js/collectedData.js
@@ -0,0 +1,9 @@
+import { time } from "./ui.js";
+import { handlePexelsApi } from "./apiFetch.js";
+// display fetched image as background image 
+const backGroundImages = await handlePexelsApi();
+document.getElementById("background-image").style.backgroundImage = `url(${backGroundImages})`;
+// show currnet time in uk
+const localTime = time();
+document.getElementById("current-date").innerHTML = localTime;
+
