61d9556a3b09fb5b223e531abc2540fbe497001d
ZigEliaZag <eliakaliza50@icloud.com>
2026-02-18 23:52:04 +0100

    display background

--- js/collectedData.js
+++ js/collectedData.js
@@ -2,8 +2,8 @@ import { time } from "./ui.js";
 import { handlePexelsApi } from "./apiFetch.js";
 // display fetched image as background image 
 const backGroundImages = await handlePexelsApi();
-console.log(backGroundImages)
-//document.getElementById("background-image").style.backgroundImage = `url(${backGroundImages})`;
+//console.log(backGroundImages)
+document.getElementById("background-image").style.backgroundImage = `url(${backGroundImages})`;
 // show currnet time in uk
 const localTime = time();
 document.getElementById("current-date").innerHTML = localTime;
