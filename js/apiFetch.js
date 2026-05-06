a7e15b21a1c42f4bf1ee8760fdba3386d812daa8
ZigEliaZag <eliakaliza50@icloud.com>
2026-05-06 11:38:11 +0200

    deleted commneted out lines, not needed anymore

--- js/apiFetch.js
+++ js/apiFetch.js
@@ -1,4 +1,4 @@
-
+import { getJson } from "serpapi";
 // fetch api keys from server.js
 let config = {};
 async function getEnvKeys() {
@@ -46,90 +46,6 @@ const manchesterApiSummary = await manchesterSummary();
 // display api fetched content
 document.getElementById("londonSummary").innerHTML = londonApiSummary;
 document.getElementById("manchesterSummary").innerHTML = manchesterApiSummary;
-
-
-//// pexels image url
-//const pexelsUrl = 'https://api.pexels.com/v1/photos';
-//const pexelsKey = config.pexelsapiKey; // pexels api key
-//const pexelsImgId = 1181202;// pexels image id 
-//
-//// fetch pexels image
-//export async function handlePexelsApi() {
-//    /**
-//     fetch the pexels api photo and return the result
-//     */
-//    const getData = await fetch(`${pexelsUrl}/${pexelsImgId}`, {
-//        method: 'GET',
-//        id: pexelsImgId,
-//        headers: {
-//            Authorization: pexelsKey// pexels api key
-//            // personal key to access the api
-//        }
-//    });
-//    const displayData = await getData.json();
-//
-//    return displayData.src.original;
-//    //console.log(displayData.src.original)
-//
-//}
-
-// departure station 
-//const departureTrainStation = 'EUS'; // London Euston 
-//
-//async function trainUrl() {
-//    /**
-//     fetch api data.  
-//     when status === 403 it will result in many errors in this fucntion, so this does not mean other lines of codes 
-//     are wrong. error will desappear when after 24h
-//     */
-//    const trainData = await fetch(`https://transportapi.com/v3/uk/train/station_timetables/${departureTrainStation}.json?app_id=${config.transportapiId}&app_key=${config.transportapiKey}&train_status=passenger`);
-//    // show error message when usage limit of the allowed data is above 100% 
-//    // limit utilization hits per day is 30. every laod equal 1 hit
-//    if (trainData.status === 403) {
-//        errorMessage(`${trainData.status}: Limit usage is above 100%. Limit utilization of hits per day: 30/30. You have to wait 24H`);
-//        console.error("forbidden request")
-//    }
-//    function errorMessage(apiErrorMessage) {
-//        /** display error message to the user */
-//        const diplayError = document.getElementById("transportApiErrorMessage");
-//        diplayError.textContent = apiErrorMessage;
-//    }
-//
-//    const response = await trainData.json();
-//    // gets all allowed data by api provider
-//    const departure = response.departures.all;
-//
-//    // collect all the wished data and returns it in tr 
-//    const timeTable = departure.map(departure => {
-//        return `
-//        <tr>
-//           <td>${departure.aimed_departure_time}</td>
-//           <td>${departure.origin_name}</td>
-//           <td>${departure.destination_name}</td>
-//           <td>${departure.platform}</td>
-//           <td>${departure.train_uid}</td>
-//        </tr>
-//        `
-//    }).join('\n'); // join table 
-//
-//    // Names in th table and append all the returned data from departure
-//    const showTable = `
-//      <table>
-//        <tr>
-//          <th>Departs</th>
-//          <th>Departure</th>
-//          <th>Destination</th>
-//          <th>Platform</th>
-//          <th>Train</th>
-//        </tr>
-//        ${timeTable /** appends returned tr data from departure to the table */}
-//      </table>
-//    `;
-//    // display the table in html file
-//    document.getElementById("timeTable").innerHTML = showTable;
-//
-//}
-//trainUrl();
 
 //personal ticketmaster api key
 const ticketsApiKey = config.ticketmasterapiKey;
